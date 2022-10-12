771a96cae681cd6695c5eab95cd66904322d06e0
sheng.zeng <sheng.zeng@aitronx.com>
2022-10-12 20:26:05 +0800

    pass by ref

--- c1_unique_pointer/main.cpp
+++ c1_unique_pointer/main.cpp
@@ -10,18 +10,35 @@ void do_with_cat_pass_value(std::unique_ptr<Cat> c)
     c->cat_info();
 }
 
+void do_with_cat_pass_ref(std::unique_ptr<Cat> &c)
+{
+    c->set_cat_name("oo");
+    c->cat_info();
+    c.reset();
+}
+
+std::unique_ptr<Cat> get_unique_ptr()
+{
+    std::unique_ptr<Cat> p_dog = std::make_unique<Cat>("Local cat");
+    return p_dog;
+}
+
 int main()
 {
-    // pass by value
-    std::unique_ptr<Cat> c1 = make_unique<Cat>("ff");
+    // // 1. pass by value
+    // std::unique_ptr<Cat> c1 = make_unique<Cat>("ff");
+    // // do_with_cat_pass_value(c1); // error
+    // do_with_cat_pass_value(std::move(c1)); // right
+    // // c1->cat_info(); //error
+    // // make_unique
+    // do_with_cat_pass_value(std::make_unique<Cat>());
 
-    // do_with_cat_pass_value(c1); // error
-    do_with_cat_pass_value(std::move(c1)); // right
-    // c1->cat_info(); //error
-    // make_unique
-    do_with_cat_pass_value(std::make_unique<Cat>());
-    
-
+    // 2. pass ref
+    // 不加const
+    std::unique_ptr<Cat> c2 = make_unique<Cat>("f2");
+    do_with_cat_pass_ref(c2);
+    // c2->cat_info();
+    cout << "address : " << c2.get() <<endl;
 
     cout << "--------------------------" << endl;
 

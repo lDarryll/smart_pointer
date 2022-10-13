509ae4be46e2d4dc77b4c9eb3d30f5ba9d112e7b
sheng.zeng <sheng.zeng@aitronx.com>
2022-10-13 18:38:18 +0800

    shared_pointer 自定义类型

--- c2_shared_pointer/main.cpp
+++ c2_shared_pointer/main.cpp
@@ -6,32 +6,49 @@ using namespace std;
 int main()
 {
 
-    // 常量类型
-    std::shared_ptr<int> i_p_1 = make_shared<int>(10);
-    cout << "value : " << *i_p_1 << endl;
-    cout << "use count : " << i_p_1.use_count() << endl;
+    // // 常量类型
+    // std::shared_ptr<int> i_p_1 = make_shared<int>(10);
+    // cout << "value : " << *i_p_1 << endl;
+    // cout << "use count : " << i_p_1.use_count() << endl;
 
-    // copy
-    std::shared_ptr<int> i_p_2 = i_p_1;
-    cout << "i_p_1 value : " << *i_p_1 << endl;
-    cout << "i_p_2 value : " << *i_p_2 << endl;
-    cout << "i_p_1 use count : " << i_p_1.use_count() << endl;
-    cout << "i_p_2 use count : " << i_p_2.use_count() << endl;
+    // // copy
+    // std::shared_ptr<int> i_p_2 = i_p_1;
+    // cout << "i_p_1 value : " << *i_p_1 << endl;
+    // cout << "i_p_2 value : " << *i_p_2 << endl;
+    // cout << "i_p_1 use count : " << i_p_1.use_count() << endl;
+    // cout << "i_p_2 use count : " << i_p_2.use_count() << endl;
 
-    // change
-    *i_p_2 = 30;
-    cout << "i_p_1 value : " << *i_p_1 << endl;
-    cout << "i_p_2 value : " << *i_p_2 << endl;
+    // // change
+    // *i_p_2 = 30;
+    // cout << "i_p_1 value : " << *i_p_1 << endl;
+    // cout << "i_p_2 value : " << *i_p_2 << endl;
 
-    std::shared_ptr<int> i_p_3 = i_p_1;
-    i_p_1 = nullptr;
-    i_p_2 = nullptr;
-    cout << "i_p_1 use count : " << i_p_1.use_count() << endl;
-    cout << "i_p_2 use count : " << i_p_2.use_count() << endl;
-    cout << "i_p_3 use count : " << i_p_3.use_count() << endl;
+    // std::shared_ptr<int> i_p_3 = i_p_1;
+    // i_p_1 = nullptr;
+    // i_p_2 = nullptr;
+    // cout << "i_p_1 use count : " << i_p_1.use_count() << endl;
+    // cout << "i_p_2 use count : " << i_p_2.use_count() << endl;
+    // cout << "i_p_3 use count : " << i_p_3.use_count() << endl;
+
+    // 自定义类型
+    std::shared_ptr<Cat> c_p_1 = make_shared<Cat>();
+    cout << "c_p_1 use count : " << c_p_1.use_count() << endl;
+    std::shared_ptr<Cat> c_p_2 = c_p_1;
+    cout << "c_p_2 use count : " << c_p_2.use_count() << endl;
+    std::shared_ptr<Cat> c_p_3 = c_p_1;
+    cout << "c_p_3 use count : " << c_p_3.use_count() << endl;
+    cout << "c_p_1 use count : " << c_p_1.use_count() << endl;
+
+    cout << "-----------split line---------------" << endl;
+
+    c_p_1.reset();
+    cout << "c_p_1 use count : " << c_p_1.use_count() << endl;
+    cout << "c_p_2 use count : " << c_p_2.use_count() << endl;
+    cout << "c_p_3 use count : " << c_p_3.use_count() << endl;
 
 
-    cout << "--------------------------" << endl;
+
+    cout << "-------------finish-------------" << endl;
 
     return 0;
 }
